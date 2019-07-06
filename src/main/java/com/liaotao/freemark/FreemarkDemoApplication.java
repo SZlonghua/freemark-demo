f469f32608d8006379c57989105d49fbcfeaa8e9
SZlonghua <1053595189@qq.com>
2019-07-06 19:44:37 +0800

    初始化

---
+++ src/main/java/com/liaotao/freemark/FreemarkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.liaotao.freemark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreemarkDemoApplication.class, args);
+	}
+
+}
