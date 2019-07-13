c2c1e6c3e080ef4d26bb70c60fe45072102afbf2
SZlonghua <1053595189@qq.com>
2019-07-13 21:05:34 +0800

    脚本 打包

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "chmod user"
-chmod 777 /root/demo/freemark-demo-0.0.1-SNAPSHOT.jar
+chmod 777 /root/demo/*
 echo "execute...."
 cd /root/demo/
 nohup /usr/java/jdk1.8.0_144/bin/java -jar freemark-demo-0.0.1-SNAPSHOT.jar  > /dev/null &
