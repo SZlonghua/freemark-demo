c47afb3eafb3356fed2c9178dd204bbc910b4bb1
SZlonghua <1053595189@qq.com>
2019-07-13 21:23:42 +0800

    脚本 打包

--- bin/start.sh
+++ bin/start.sh
@@ -7,4 +7,4 @@ cd /root/demo/
 nohup /usr/java/jdk1.8.0_144/bin/java -jar freemark-demo-0.0.1-SNAPSHOT.jar  > /dev/null &
 pidnew=`ps -ef | grep freemark-demo-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}'`
 echo "new process id: $pidnew"
-echo "start success"
+echo "start success!"
